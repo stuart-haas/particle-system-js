fdc3bc6c288484cdb22fc66184a737a3467d6765
Stuart Haas <stuarthaas@stuarts-mbp-2.home>
2018-08-17 16:58:42 -0400

    Add plumber and livereload to gulp js task

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,30 @@
 const gulp = require('gulp');
+const gutil = require('gulp-util');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
 const uglify = require('gulp-uglify');
 const strip = require('gulp-strip-comments');
-const livereload = require('gulp-livereload');
 const webpack = require('webpack-stream');
 const webpackConfig = require('./webpack.config.js');
+const livereload = require('gulp-livereload');
 
 const JS_SRC = 'src/js/app/**/*.js';
 const JS_DEST = 'public/js';
 
 gulp.task('js', function(){
   return gulp.src(JS_SRC)
+    .pipe(plumber({ errorHandler: function(err) {
+      notify.onError({
+        title: "Gulp error in " + err.plugin,
+        message:  err.toString()
+      })(err);
+      gutil.beep();
+    }}))
     .pipe(webpack(webpackConfig))
     .pipe(uglify())
     .pipe(strip())
     .pipe(gulp.dest(JS_DEST))
+    .pipe(livereload());
 });
 
 gulp.task('watch', function() {
