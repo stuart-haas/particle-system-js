8885c868b83c93fe7c17426c25126c3ccf1bc70e
Stuart Haas <stuarthaas@stuarts-mbp-2.home>
2018-08-18 03:57:30 -0400

    Set up particle system

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,3 +1,13 @@
-import Particle from './objects/particle';
+import Vector from './math/vector';
+import Emitter from './core/emitter';
+import Field from './core/field';
+import ParticleSystem from './core/particlesystem';
 
-var particle = new Particle(4, 4);
+let particlesystem = new ParticleSystem();
+
+particlesystem.addEmitter(new Emitter(Emitter.TYPE.AREA, new Vector(particlesystem.canvas.width, particlesystem.canvas.height), Vector.fromAngle(1, 1), 1, 20000, 20, 0.98, 100, 2, Emitter.COLOR.RANDOM));
+
+particlesystem.addField(new Field(new Vector(200, 400), -500));
+particlesystem.addField(new Field(new Vector(800, 400), 500));
+
+particlesystem.start();
