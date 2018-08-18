cc88f69b17ee2d8d3c4b44e6e26aa58c09392378
Stuart Haas <stuarthaas@stuarts-mbp-2.home>
2018-08-18 03:56:55 -0400

    And type and color utility classes

---
+++ src/js/app/utils/color.js
@@ -0,0 +1,7 @@
+class Color {
+    static get RANDOM() {
+        return 'random';
+    }
+}
+
+export {Color as default}
---
+++ src/js/app/utils/type.js
@@ -0,0 +1,11 @@
+class Type {
+    static get POINT() {
+        return 'point';
+    }
+
+    static get AREA() {
+        return 'area';
+    }
+}
+
+export {Type as default}
